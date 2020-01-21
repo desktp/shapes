7ff0c9c073cabedee23c654715d7f15f935dbe69
jacques mouette <j.mouette@gmail.com>
2020-01-21 20:47:55 -0300

    Implement fourth point calculation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Point from './components/Point';
 import Button from './components/Button';
 
 const App = () => {
   const [points, setPoints] = useState([]);
+  const [finalPoint, setFinalPoint] = useState({});
+
+  useEffect(() => {
+    // if we have all 3 points, calculate final point
+    if (points.length === 3) {
+      /**
+      * considering we have a parallelogram with vertexes A, B, C
+      * we can get the last vertex's (D) coordinates with
+      *  Dx = Ax + Cx - Bx
+      *  Dy = Ay + Cy - By
+      *  https://www.geeksforgeeks.org/find-missing-point-parallelogram/
+      **/
+      setFinalPoint({
+        left: points[0].left + (points[2].left - points[1].left),
+        top: points[0].top + (points[2].top - points[1].top),
+      });
+    }
+  }, [points])
 
   const handleClick = (e) => {
     if (points.length < 3) {
-      setPoints([...points, { top: e.clientY, left: e.clientX }]);
+      const top = e.clientY;
+      const left = e.clientX;
+      setPoints([...points, { top, left }]);
     }
   }
 
   const handleReset = (e) => {
     e.stopPropagation();
     setPoints([]);
+    setFinalPoint([]);
   }
 
   return (
     <div className="App" onClick={handleClick}>
       <Button onClick={handleReset} message={"Reset"} />
       {points.map(point => <Point top={point.top} left={point.left} />)}
+      {finalPoint.top && <Point top={finalPoint.top} left={finalPoint.left} final />}
     </div>
   );
 }
--- src/components/Point.js
+++ src/components/Point.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Point = ({ top, left }) => (
+const Point = ({ top, left, final }) => (
   <div style={{
     position: "absolute",
     top,
@@ -10,7 +10,7 @@ const Point = ({ top, left }) => (
     // 9px + 2px from the border equals 11px as requested
     width: 9,
     height: 9,
-    border: "1px solid red",
+    border: `1px solid ${final ? "blue" : "red"}`,
     borderRadius: 100
   }}>
     <p style={{ fontSize: 8, color: "white", whiteSpace: "nowrap" }}>{`x: ${left}, y: ${top}`}</p>
