7d01fda5f2fddf6f49c4dfe9eb699b5c9a9eccf0
jacques mouette <j.mouette@gmail.com>
2020-01-23 11:32:24 -0300

    Draw parallelogram

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Button from './components/Button';
 const App = () => {
   const [points, setPoints] = useState([]);
   const [finalPoint, setFinalPoint] = useState({});
+  const [dragStatus, setDragging] = useState({ dragging: false, index: 0 });
 
   useEffect(() => {
     // if we have all 3 points, calculate final point
@@ -22,7 +23,28 @@ const App = () => {
         top: points[0].top + (points[2].top - points[1].top),
       });
     }
-  }, [points])
+  }, [points]);
+
+  useEffect(() => {
+    if (finalPoint.top) drawParallelogram();
+  }, [finalPoint]);
+
+  const drawParallelogram = () => {
+    var c = document.querySelector("canvas");
+    // Set canvas size to match screen
+    c.width = document.body.clientWidth; //document.width is obsolete
+    c.height = document.body.clientHeight; //document.height is obsolete
+ 
+    var ctx = c.getContext("2d");
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo(points[0].left, points[0].top);
+    ctx.lineTo(points[1].left, points[1].top);
+    ctx.lineTo(points[2].left, points[2].top);
+    ctx.lineTo(finalPoint.left, finalPoint.top);
+    ctx.lineTo(points[0].left, points[0].top);
+    ctx.stroke();
+  }
 
   const handleClick = (e) => {
     if (points.length < 3) {
@@ -36,14 +58,39 @@ const App = () => {
     e.stopPropagation();
     setPoints([]);
     setFinalPoint([]);
+    var c = document.querySelector("canvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+  }
+
+  const startDrag = (pointIndex) => (e) => {
+    e.stopPropagation();
+    setDragging({ dragging: true, index: pointIndex });
+  }
+
+  const stopDrag = () => setDragging({ dragging: false, index: 0 });
+
+  const handleDrag = (e) => {
+    if (!dragStatus.dragging || points.length < 3) return;
+
+    // Spread to new array to avoid mutating
+    const newPoints = [...points];
+    const newCoords = { top: e.clientY, left: e.clientX };
+
+    // Replace point being dragged with new coordinates
+    newPoints.splice(dragStatus.index, 1, newCoords);
+    setPoints(newPoints);
   }
 
   return (
-    <div className="App" onClick={handleClick}>
-      <Button onClick={handleReset} message={"Reset"} />
-      {points.map(point => <Point top={point.top} left={point.left} />)}
-      {finalPoint.top && <Point top={finalPoint.top} left={finalPoint.left} final />}
-    </div>
+    <>
+      <div className="App" onClick={handleClick} onMouseMove={handleDrag}>
+        <canvas height="100vh" width="100vh" className="root-canvas" />
+        <Button onClick={handleReset} message={"Reset"} />
+        {points.map((point, index) => <Point key={point.top} top={point.top} left={point.left} onMouseDown={startDrag(index)} onMouseUp={stopDrag} draggable={points.length === 3} />)}
+        {finalPoint.top && <Point top={finalPoint.top} left={finalPoint.left} final />}
+      </div>
+    </>
   );
 }
 
--- src/components/Point.js
+++ src/components/Point.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-const Point = ({ top, left, final }) => (
-  <div style={{
-    position: "absolute",
-    top,
-    left,
-    marginLeft: -7,
-    marginTop: -7,
-    // 9px + 2px from the border equals 11px as requested
-    width: 9,
-    height: 9,
-    border: `1px solid ${final ? "blue" : "red"}`,
-    borderRadius: 100
-  }}>
+const Point = ({ top, left, final, onMouseDown, onMouseUp, draggable = false }) => (
+  <div
+    style={{
+      position: "absolute",
+      top,
+      left,
+      marginLeft: -7,
+      marginTop: -7,
+      // 9px + 2px from the border equals 11px as requested
+      width: 9,
+      height: 9,
+      border: `1px solid ${final ? "blue" : "red"}`,
+      borderRadius: 100,
+      cursor: draggable ? "grab" : "default"
+    }}
+    onMouseDown={draggable ? onMouseDown : () => {}}
+    onMouseUp={onMouseUp}
+  >
     <p style={{ fontSize: 8, color: "white", whiteSpace: "nowrap" }}>{`x: ${left}, y: ${top}`}</p>
   </div>
 );
