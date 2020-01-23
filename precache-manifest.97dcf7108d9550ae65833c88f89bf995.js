ea9286e2b915013472a975f04af35914e4510629
jacques mouette <j.mouette@gmail.com>
2020-01-23 16:15:12 -0300

    Updates

---
+++ precache-manifest.97dcf7108d9550ae65833c88f89bf995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11596674a152dc6a13d5b7766b05d29",
+    "url": "/shapes/index.html"
+  },
+  {
+    "revision": "87c75bfcd336a6955a4d",
+    "url": "/shapes/static/css/main.fd585ac1.chunk.css"
+  },
+  {
+    "revision": "e451489fd94346de7c78",
+    "url": "/shapes/static/js/2.db6574a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shapes/static/js/2.db6574a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87c75bfcd336a6955a4d",
+    "url": "/shapes/static/js/main.77e11f5d.chunk.js"
+  },
+  {
+    "revision": "f2fe7fed6935bae0ddc5",
+    "url": "/shapes/static/js/runtime-main.1cb1d172.js"
+  }
+]);
